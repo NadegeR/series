af967fe4f86fa55703a84ea7d7898d0fce29e8f6
Nadege Rampon <nadegerampon@yahoo.fr>
2022-05-04 15:17:59 +0200

    creer les pages de base

---
+++ src/Controller/SerieController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\Serie;
+use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/series", name="serie_")
+ */
+
+class SerieController extends AbstractController
+{
+    /**
+     * @Route("", name="list")
+     */
+    public function list(SerieRepository $serieRepository): Response
+    {
+       // $series = $serieRepository->findBy([], ['popularity' => 'DESC', 'vote' => 'DESC'], 30);
+        $series = $serieRepository->findBestSeries();
+
+        return $this->render('serie/list.html.twig',["series" => $series]);
+    }
+
+    /**
+     * @Route("/details/{id}", name="details")
+     */
+    public function details(int $id, SerieRepository $serieRepository): Response
+    {
+        $serie = $serieRepository->find($id);
+        return $this->render('serie/details.html.twig', ["serie" => $serie]);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     */
+    public function create(Request $request ): Response
+    {
+        dump($request);
+        return $this->render('serie/create.html.twig');
+    }
+
+    /**
+     * @Route ("/demo", name="em-demo")
+     */
+    public function demo(EntityManagerInterface $entityManager): Response{
+//        //creation de l'instance de mon entite
+//        $serie = new Serie();
+
+//        //hydrate toutes le ppte
+//        $serie->setName('pif');
+//        $serie->setBackdrop('dafsd');
+//        $serie->setPoster('dafsd');
+//        $serie->setDateCreated(new \DateTime());
+//        $serie->setFirstAirDate(new \DateTime("-1 year"));
+//        $serie->setLastAirDate(new \DateTime("-6 month"));
+//        $serie->setGenres('drama');
+//        $serie->setOverview('bla bla bla');
+//        $serie->setPopularity(123.00);
+//        $serie->setVote(8.2);
+//        $serie->setStatus('Canceled');
+//        $serie->setTmdbId(329432);
+//        dump($serie);
+//
+//        $entityManager->persist($serie);
+//        $entityManager->flush();
+//
+//        dump($serie);
+//        $serie->setGenres('comedy');
+//        $entityManager->remove($serie); pour supprimer
+//        $entityManager->flush();
+
+        return $this->render('serie/create.html.twig');
+    }
+}
---
+++ src/Repository/SerieRepository.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Serie;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Serie>
+ *
+ * @method Serie|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Serie|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Serie[]    findAll()
+ * @method Serie[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SerieRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Serie::class);
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function add(Serie $entity, bool $flush = true): void
+    {
+        $this->_em->persist($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function remove(Serie $entity, bool $flush = true): void
+    {
+        $this->_em->remove($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    public function findBestSeries(){
+        //en DQL
+//        $entityManager = $this->getEntityManager();
+//        $dql = "SELECT s
+//                FROM App\Entity\Serie s
+//                WHERE s.popularity > 100
+//                AND s.vote > 8
+//                ORDER BY s.popularity DESC";
+//        $query = $entityManager->createQuery($dql);
+//        $query->setMaxResults(50);
+//        $results = $query->getResult();
+//
+//        return $results;
+
+        //en QueryBuilder
+        $queryBuilder = $this->createQueryBuilder('s');
+        $queryBuilder->andWhere('s.popularity >100');
+        $queryBuilder->andWhere('s.vote > 8');
+        $queryBuilder->addOrderBy('s.popularity', 'DESC');
+        $query = $queryBuilder->getQuery();
+        $results = $query->getResult();
+
+        return $results;
+    }
+
+    // /**
+    //  * @return Serie[] Returns an array of Serie objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Serie
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
